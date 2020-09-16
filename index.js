918922c924f8e15e47032f27780cc680d7c087cb
kevinpark07 <kevin.park07@yahoo.com>
2020-09-16 10:34:54 -0400

    stored commands

--- index.js
+++ index.js
@@ -3,4 +3,41 @@ document.addEventListener("DOMContentLoaded", function(){
   renderBot(currentPosition)
 
   // ADD CODE HERE!
+  function storeCommands() {
+    const movementContainer = document.querySelector('#moves-container')
+
+    document.addEventListener("keydown", function(e) {
+      if(e.key === "ArrowLeft") {
+        const movementLi = document.createElement('li')
+        const direction = "left"
+        movementLi.textContent = direction
+        movementContainer.appendChild(movementLi);
+      } 
+      if(e.key === "ArrowRight") {
+        const movementLi = document.createElement('li')
+        const direction = "right"
+        movementLi.textContent = direction
+        movementContainer.appendChild(movementLi);
+      } 
+      if(e.key === "ArrowUp") {
+        const movementLi = document.createElement('li')
+        const direction = "up"
+        movementLi.textContent = direction
+        movementContainer.appendChild(movementLi);
+      } 
+      if(e.key === "ArrowDown") {
+        const movementLi = document.createElement('li')
+        const direction = "down"
+        movementLi.textContent = direction
+        movementContainer.appendChild(movementLi);
+      } 
+
+      
+
+      
+      
+    })
+  }
+
+  storeCommands()
 })
